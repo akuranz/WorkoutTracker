66c0d93bc329596eb0517c980be02091133a73f5
Abby Kuranz <akuranz@gmail.com>
2020-03-14 09:29:59 -0600

    removed mongoose connect before creating heroku

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // db.Excercise.create();
 require("./routes/api-routes")(app);
